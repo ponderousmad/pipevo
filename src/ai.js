dee591ed91add8fbf8d17286dfcc313b9bb4e358
Adrian Smith <agnomen@gmail.com>
2016-02-20 17:17:51 -0500

    Port AI players to JavaScript.
    None of this has been tested - still just syntax level porting.

--- src/ai.js
+++ src/ai.js
@@ -59,7 +59,7 @@ var AI = (function () {
         return result;
     }
     
-    function findEmptyPosFarFromOutput(game, overlay, followResult) {
+    function farthestEmptyFromOutput(game, overlay, followResult) {
         var board = overlay ? overlay : game.substrate,
             follow = followResult ? followResult : followPipe(game, board),
             bestDistance = -1,
@@ -182,7 +182,191 @@ var AI = (function () {
     }
     
     FarDiscarder.prototype.discard = function () {
-        return findEmptyPosFarFromOutput(this.game, this.board);
+        return farthestEmptyFromOutput(this.game, this.board);
+    };
+    
+    function DiscarderAI(game, Discarder) {
+        this.discarder = new Discarder(game);
+        this.version = 1;
+    }
+    
+    DiscarderAI.prototype.nextMove = function () {
+        return this.discarder.discard();
+    };
+    
+    function makeDiscarder(game, stopFlow, Discarder) {
+        if (stopFlow) {
+            game.setInfiniteTimeToFlow();
+        }
+        return new DiscarderAI(game, Discarder);
+    }
+    
+    function FollowerAI(game, Discarder) {
+        this.game = game;
+        this.discarder = makeDiscarder(game, false, Discarder);
+        this.version = 1;
+    }
+    
+    FollowerAI.prototype.nextMove = function () {
+        var follow = followPipe(this.game);
+		if (follow.openEnd) {
+			if (this.game.peek()[0].isPipeAt(PIPES.OPPOSITE(follow.outFlow))) {
+				return follow.position;
+			} else {
+				return this.discarder.nextMove();
+			}
+		}
+		return null;
+    };
+    
+    function makeFollower(game, Discarder) {
+        return new FollowerAI(game, Discarder);
+    }
+    
+    function Susan(game) {
+        this.game = game;
+        this.discarder = makeDiscarder(game, BorderDiscarder);
+        this.version = 2;
+    }
+
+	Susan.prototype.canPlace = function (position, flow) {
+		if (!position.valid()) {
+			return false;
+		}
+        var piece = this.game.substrate.at(position);
+        return piece === null || piece.isPipeAt(flow);
+	};
+    
+    Susan.prototype.nextMove = function () {
+        var follow = followPipe(this.game);
+
+		if (follow.openEnd) {
+			var piece = mGame.peek()[0],
+                flow = nextPiece.getFarSide(PIPES.OPPOSITE(follow.outFlow));
+			if (flow !== null) {
+				var next = follow.position.to(flow);
+				if (this.canPlace(next, flow)) {
+					return pipe.outPosition();
+				}
+			}
+            return this.discarder.discard();
+		}
+		return null;
+    };
+    
+    function Bob(game) {
+        this.game = game;
+        this.version = 8;
+        this.best = null;
+        this.bestLength = null;
+        
+        this.ALL_PIECES = [];        
+        for (var t = 0; t < PIPES.NON_SOURCE_TYPES.length; ++t) {
+            this.ALL_PIECES.push(new PIPES.Piece(PIPES.NON_SOURCE_TYPES[t]));
+        }
+        
+    }
+    
+    Bob.prototype.updateBest = function	(position, length) {
+		if (length > this.bestLength ) {
+			this.best = position;
+			this.bestLength = length;
+		}
+	};
+    
+    Bob.prototype.buildPeeks = function () {
+		var peeks = [];
+		for (var i = 0; i < this.game.peek().length; ++i) {
+			peeks.push(i);
+		}
+		return peeks;
+	};
+
+	Bob.prototype.remainingPeeks = function (peeks, toRemove) {
+		var result = [];
+		for (var i = 0; i < peeks.length; ++i) {
+            var peek = peeks[i];
+            if (peek != toRemove) {
+				result.push(peek);
+			}
+		}
+		return result;
+	};
+
+	Bob.prototype.tryPlace = function (board, peek, target) {
+		return new Acetate(board, this.game.peek()[peek], target);
+	};
+    
+    Bob.prototype.tryPlaces = function (board, peeks, first, hint) {
+		if (!hint.valid() || !board.isEmpty(hint)) {
+			return;
+		}
+
+		var discard = null,
+            attempt = first;
+            
+		for (var p = 0; p < peeks.length; ++p) {
+            var peek = peeks[p];
+			var target = hint,
+                overlay = tryPlace(board, peek, target),
+                follow = followPipe(this.game, overlay);
+
+			if (!follow.isEndOpen) {
+                if (discard === null) {
+                    discard = farthestEmptyFromOutput(this.game, overlay);
+                }
+				target = discard;
+				overlay = tryPlace(board, peek, target);
+				follow = PipeFollower.follow(this.game, overlay);
+			}
+            if (peek === 0) {
+                attempt = target;
+            }
+			this.tryPlaces(overlay, this.remainingPeeks(peeks, peek), attempt, follow.position);
+			this.updateBest(attempt, follow.length);
+		}
+	};
+    
+    Bob.prototype.followPipe = function (board, piece, position) {
+		return followPipe(this.game, new Acetate(board, piece, position));
+	};
+    
+    Bob.prototype.findMaxPipeLength = function(board, position) {
+		var maxLength = 0;
+		for (var t = 0; t < this.ALL_PIECES.length; ++t) {
+            piece = this.ALL_PIECES[t];
+			var follow = followPipe(board, piece, position);
+			if (follow.isEndOpen) {
+				return this.game.width() * this.game.height();
+			}
+			if (follow.length > maxLength) {
+				maxLength = follow.length;
+			}
+		}
+		return maxLength;
+	};
+
+	Bob.prototype.isNoPoint = function (board, piece, position) {
+		return this.followPipe(board, piece, position).length === this.findMaxPipeLength(board, position);
+	};
+    
+    Bob.prototype.nextMove = function () {
+        var follow = followPipe(this.game);
+        if( !follow.isEndOpen ) {
+            return null;
+		}
+		if (this.isNoPoint(this.game.substrate, this.game.peek()[0], follow.position)) {
+			return follow.position;
+		}
+
+		this.bestLength = 0;
+		this.best = null;
+		this.tryPlaces(this.game.substrate, this.buildPeeks(), null, follow.position);
+
+        if (this.best !== null) {
+            return this.best;
+        }
+		return farthestEmptyFromOutput(this.game);
     };
     
     return {
@@ -190,304 +374,11 @@ var AI = (function () {
         RandomDiscarder: RandomDiscarder,
         FarDiscarder: FarDiscarder,
         BorderDiscarder: BorderDiscarder,
-        followPipe: followPipe
+        followPipe: followPipe,
+        makeDiscarder: makeDiscarder,
+        makeFollower: makeFollower,
+        Susan: Susan,
+        Bob: Bob
     };
 }());
 
-/*
-public class BorderAI implements PipeAI {
-
-	public void setGame( GamePlay game, long seed ) {
-		game.setInfiniteTimeToFlow();
-		mGame = game;
-		mDiscarder = new BorderDiscarder( game );
-	}
-
-	public boolean performMove() {
-		return mGame.placeNext( mDiscarder.discard() );
-	}
-
-	public Player player() {
-		return Player.BORDER;
-	}
-
-	private GamePlay mGame;
-	private Discarder mDiscarder;
-}
-
-public class FarAI implements PipeAI {
-
-	public void setGame( GamePlay game, long seed ) {
-		game.setInfiniteTimeToFlow();
-		mGame = game;
-		mDiscarder = new FarDiscarder( game );
-
-	}
-
-	public boolean performMove() {
-		return mGame.placeNext( mDiscarder.discard() );
-	}
-
-	public Player player() {
-		return Player.FAR;
-	}
-
-	private GamePlay mGame;
-	private Discarder mDiscarder;
-}
-
-public class RandomAI implements PipeAI {
-	public void setGame( GamePlay game, long seed ) {
-		mGame = game;
-		mDiscarder = new RandomDiscarder( game, seed );
-	}
-
-	public boolean performMove() {
-		return mGame.placeNext( mDiscarder.discard() );
-	}
-
-	private GamePlay mGame;
-	private Discarder mDiscarder;
-
-	public Player player() {
-		return Player.RANDOM;
-	}
-}
-
-public class RandomEndAI implements PipeAI {
-
-	public void setGame( GamePlay game, long seed ) {
-		mGame = game;
-		mRandom.setGame( game, seed );
-	}
-
-	public boolean performMove() {
-		PipeFollower pipe = new PipeFollower( mGame );
-
-		if( pipe.follow() ) {
-			if( mGame.peek()[0].isPipeAt( pipe.outDirection().opposite() ) ) {
-				mGame.placeNext( pipe.outPosition() );
-			} else {
-				return mRandom.performMove();
-			}
-			return true;
-		}
-		return false;
-	}
-
-	public Player player() {
-		return Player.RANDOM_END;
-	}
-
-	GamePlay mGame = null;
-	RandomAI mRandom = new RandomAI();
-}
-
-public class Susan implements PipeAI {
-
-	public void setGame( GamePlay game, long seed ) {
-		mGame = game;
-		mDiscarder = new BorderDiscarder( game );
-	}
-
-	public boolean performMove() {
-		PipeFollower pipe = new PipeFollower( mGame );
-
-		if( pipe.follow() ) {
-			Piece nextPiece = mGame.peek()[0];
-			if( nextPiece.getFarSide( pipe.outDirection().opposite() ) != null ) {
-				Side outDirection = nextPiece.getFarSide( pipe.outDirection().opposite() );
-				Position nextOut = pipe.outPosition().to( outDirection );
-				if( canPlace( nextOut, outDirection ) ) {
-					mGame.placeNext( pipe.outPosition() );
-				} else {
-					return discard();
-				}
-			} else {
-				return discard();
-			}
-			return true;
-		}
-		return false;
-	}
-
-	private boolean discard()
-	{
-		return mGame.placeNext( mDiscarder.discard() );
-	}
-
-	private boolean canPlace( Position position, Side direction ) {
-		if( !position.valid() ) {
-			return false;
-		}
-		Substrate substrate = mGame.substrate();
-		return substrate.isEmpty( position ) ||	canFlowInto( substrate.at( position ), direction );
-	}
-
-
-
-	private boolean canFlowInto(Piece piece, Side fromDirection ) {
-		return piece.isPipeAt( fromDirection.opposite() );
-	}
-
-	public Player player() {
-		return Player.SUSAN;
-	}
-
-	private GamePlay mGame;
-	private Discarder mDiscarder;
-}
-
-public class Bob implements PipeAI {
-	static private class Attempt {
-		private Position mPosition = null;
-
-		Attempt( Position target ) {
-			mPosition = target;
-		}
-
-		void set( int index, Position target ) {
-			if(index == 0) {
-				mPosition = target;
-			}
-		}
-
-		Position target() {
-			return mPosition;
-		}
-	};
-
-	private GamePlay mGame;
-	private Position mBest;
-	private double mBestLength;
-
-	private void updateBest( Position position, double length ) {
-		if( length > mBestLength ) {
-			mBest = position;
-			mBestLength = length;
-		}
-	}
-
-	private List<Integer> buildPeeks() {
-		List<Integer> peek = new ArrayList<Integer>();
-		for( int i = 0; i < mGame.peek().length; i++ ) {
-			peek.add(i);
-		}
-		return peek;
-	}
-
-	private List<Integer> remainingPeeks(List<Integer> peeks, int toRemove) {
-		List<Integer> result = new ArrayList<Integer>();
-		for (int peek: peeks) {
-			if (peek != toRemove) {
-				result.add(peek);
-			}
-		}
-		return result;
-	}
-
-	Position discard( Stracetate stracetate ) {
-		return new FarDiscarder(mGame, stracetate).discard();
-	}
-
-	private class Discard {
-		// Optimization to prevent recomputing the discard location if it is needed
-		// multiple times at the same recursion depth.
-		public Discard( Stracetate stracetate ) { mStracetate = stracetate; }
-
-		public Position position() {
-			if( mDiscard == null ) {
-				mDiscard = discard(mStracetate);
-			}
-			return mDiscard;
-		}
-		private Stracetate mStracetate;
-		private Position mDiscard = null;
-	}
-
-	private Acetate tryPlace(Stracetate stracetate, int peek, Position target) {
-		return new Acetate( stracetate, mGame.peek()[peek], target );
-	}
-
-	private void tryPlaces(Stracetate stracetate, List<Integer> peeks, Position first, Position hint) {
-		if( !hint.valid() || !stracetate.isEmpty( hint ) ) {
-			return;
-		}
-
-		Discard discard = new Discard( stracetate );
-		Attempt attempt = new Attempt( first );
-		for( int peek : peeks ) {
-			Position target = hint;
-			Stracetate acetate = tryPlace(stracetate, peek, target);
-			PipeFollower pipe = PipeFollower.follow( mGame, acetate );
-
-			if( !pipe.isEndOpen() ) {
-				target = discard.position();
-				acetate = tryPlace(stracetate, peek, target);
-				pipe = PipeFollower.follow( mGame, acetate );
-			}
-			attempt.set(peek,target);
-			tryPlaces(acetate, remainingPeeks(peeks, peek), attempt.target(), pipe.outPosition());
-			updateBest(attempt.target(),pipe.length());
-		}
-	}
-
-	private PipeFollower followPipe(Stracetate stracetate, Piece piece, Position position) {
-		return PipeFollower.follow( mGame, new Acetate( stracetate, piece, position ) );
-	}
-
-	static Piece[] sPiecePrototypes;
-	static {
-		sPiecePrototypes = new Piece[ PieceType.nonSourceCount() ];
-		for( PieceType type : PieceType.values() ) {
-			if( !type.isSource() ) {
-				sPiecePrototypes[ type.ordinal() ] = PieceFactory.buildPiece( type );
-			}
-		}
-	}
-
-	private int findMaxPipeLength(Stracetate stracetate, Position position) {
-		int maxLength = 0;
-		for( Piece piece : sPiecePrototypes ) {
-			PipeFollower pipe = followPipe(stracetate, piece, position);
-			if( pipe.isEndOpen() ) {
-				return Integer.MAX_VALUE;
-			}
-			if( pipe.length() > maxLength ) {
-				maxLength = pipe.length();
-			}
-		}
-		return maxLength;
-	}
-
-	private boolean isNoPoint( Stracetate stracetate, Piece piece, Position position ) {
-		return followPipe(stracetate, piece, position).length() == findMaxPipeLength(stracetate, position);
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public void setGame( GamePlay game, long seed ) {
-		mGame = game;
-	}
-
-	public boolean performMove() {
-		PipeFollower pipe = PipeFollower.follow( mGame );
-		if( !pipe.isEndOpen() ) {
-			return false;
-		}
-		Stracetate stracetate = new SubstrateWrapper(mGame.substrate());
-		if( isNoPoint( stracetate, mGame.peek()[0], pipe.outPosition() ) ) {
-			return mGame.placeNext( pipe.outPosition() );
-		}
-
-		mBestLength = 0;
-		mBest = null;
-		tryPlaces(stracetate, buildPeeks(), null, pipe.outPosition());
-
-		return mGame.placeNext( mBest != null ? mBest : discard( stracetate ) );
-	}
-
-	public Player player() {
-		return Player.BOB;
-	}
-}
-*/
